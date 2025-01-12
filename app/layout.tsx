543c5bd96afaec3131837ec07f59045415db1a6c
Absterrg0 <parvj5212@gmail.com>
2025-01-12 11:01:40 +0530

    Integrated droplert v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "@/providers";
+import Droplert from "@/components/droplert/droplert";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
+        <Droplert></Droplert>
         {children}
         </Providers>
       </body>
